ec38ccab901a7362db57795cc6320ce3c0c8eaf6
almazbekovna08 <themoongirl234@gmail.com>
2024-12-30 18:09:10 +0600

    hw4

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from main.models import Main
-
-# Register your models here.
-admin.site.register(Main)
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from main.models import Main
 
 # Create your views here.
 def main(request):
-    main=Main.objects.latest("id")
+    main=Main.objects.all()
     return render(request, 'index.html', locals())
 
 
